800a222e635170b83676569e416e1ee03f5fa055
Roberto Mossetto <roberto.mossetto@gmail.com>
2023-02-27 14:07:32 +0100

    fix Header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
+	extend: {},
 	plugins: [],
+	screens: {
+		'xs': {'max': '639px'},
+		'sm': {'min': '640px', 'max': '767px'},
+		'md': {'min': '768px', 'max': '1023px'},
+		'lg': {'min': '1024px', 'max': '1279px'},
+		'xl': {'min': '1280px', 'max': '1535px'},
+		'2xl': {'min': '1536px'},
+	},
 }
